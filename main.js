fe1bc23eeda84543951267123782cc0671afbb24
travor20814 <travor20814@gmail.com>
2021-06-19 17:35:30 +0800

    add some comments

--- main.js
+++ main.js
@@ -2,10 +2,14 @@ customElements.define('element-details',
   class extends HTMLElement {
     constructor() {
       super();
+
+      // get template by id
       const template = document
         .getElementById('element-details-template')
         .content;
-      const shadowRoot = this.attachShadow({mode: 'open'})
+
+      // deep clone template child nodes
+      const shadowRoot = this.attachShadow({ mode: 'open' })
         .appendChild(template.cloneNode(true));
   }
 });
